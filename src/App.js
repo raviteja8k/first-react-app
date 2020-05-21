8d361f3adcc97f736070031c9fe553a15d8bcb3c
raviteja8k <raviteja.tk@gmail.com>
2020-05-21 12:46:14 +0530

    Added new Customer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Customer from './Customer/Customer';
 
 function App() {
   return (
     <div className="App">
       <h1 className="color-red">Hola!  The react App..</h1>
       <p className="color-green">This is a paragraph here...</p>
+      <Customer />
     </div>
   );
 }
---
+++ src/Customer/Customer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const customer = () => {
+    return <p>Details of the customer here.</p>;
+}
+
+export default customer;
