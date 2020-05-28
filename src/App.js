1d4d3bf2d0bb958e38cbad1fac0f878cf64519b0
raviteja8k <raviteja.tk@gmail.com>
2020-05-28 20:06:27 +0530

    using If condition for Display toggle

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
   state = {
-    userName: 'rabadaman45'
+    userName: 'rabadaman45',
+    showDisplay: false
   }
 
   changeUsername = (event) =>{
@@ -22,6 +23,11 @@ class App extends Component {
     });
   }
 
+  toggleDisplay = () => {
+    const display = this.state.showDisplay;
+    this.setState({showDisplay: !display});
+  }
+
   render(){   
     const headStyle = {
       fontSize: '20px',
@@ -31,11 +37,16 @@ class App extends Component {
   return (
     <div className="App">
       <h1 style={headStyle}>The UserInput/UserOutput React App</h1>
-      <UserInput changeInput={this.changeUsername} defuserName={this.state.userName}/>
-      <UserOutput  userName={this.state.userName}>Dynamic Username:</UserOutput>
-      <UserOutput  userName={this.state.userName} click={this.clickHandler.bind(this,'raspberry978')}/>
-      <UserOutput  userName={'copenhaggen4'}/>
-      <UserOutput  userName={'runaround77'}/>
+      <button onClick={this.toggleDisplay}>Toggle Display</button>
+      { this.state.showDisplay ?
+        <div>
+        <UserInput changeInput={this.changeUsername} defuserName={this.state.userName}/>
+        <UserOutput  userName={this.state.userName}>Dynamic Username:</UserOutput>
+        <UserOutput  userName={this.state.userName} click={this.clickHandler.bind(this,'raspberry978')}/>
+        <UserOutput  userName={'copenhaggen4'}/>
+        <UserOutput  userName={'runaround77'}/>
+        </div> : null
+      }
     </div>
   );
 }
